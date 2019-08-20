03c4b0d56f38dcd813562a86df3825ceff7557d3
Joris Ros <j.ros@youwe.nl>
2019-08-20 14:05:16 +0200

    Add functionality for transfer complete directory

--- Command/TransferFileCommand.php
+++ Command/TransferFileCommand.php
@@ -2,12 +2,15 @@
 
 namespace FileTransferBundle\Command;
 
+use FileTransferBundle\Service\FileTransferService;
 use Pimcore\Console\AbstractCommand;
 use Pimcore\Console\Dumper;
+use Pimcore\Log\ApplicationLogger;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 
 class TransferFileCommand extends AbstractCommand
 {
@@ -41,7 +44,59 @@ class TransferFileCommand extends AbstractCommand
 
         $service = $this->getContainer()->get('FileTransferBundle\Service\FileTransferService');
 
-        $service->transferFile($targetserverid, $sourcefile, $targetfile);
+        if ($this->useDirectoryMode($sourcefile)) {
+            $this->transferDirectory($service, $targetserverid, $sourcefile, $targetfile);
+        } else {
+            $service->transferFile($targetserverid, $sourcefile, $targetfile);
+        }
+    }
+
+    /**
+     * Checks if the transfer is in directory mode
+     *
+     * @param string $source
+     * @param string $target
+     * @return bool
+     */
+    private function useDirectoryMode(string $source): bool
+    {
+        if (is_dir($source)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Transfers the a complete directory to a remote server
+     *
+     * @param FileTransferService $service
+     * @param string $serverId
+     * @param string $source
+     * @param string $target
+     */
+    private function transferDirectory(FileTransferService $service, string $serverId, string $source, string $target): void
+    {
+        $finder = new Finder();
+        $finder->files()->in($source);
+
+        /** @var ApplicationLogger $logger */
+        $logger = $this->getContainer()->get('monolog.logger.admin');
+        if (!$finder->hasResults()) {
+            $logger->notice('no files found', [
+                'component' => 'FileTransfer'
+            ]);
+            return;
+        }
+
+        foreach ($finder as $file) {
+            $destination = $target . DIRECTORY_SEPARATOR . $file->getFilename();
+
+            $service->transferFile(
+                $serverId,
+                $file,
+                $destination);
+        }
     }
 
 }
