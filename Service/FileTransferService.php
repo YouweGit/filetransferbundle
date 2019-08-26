ef112b75cde96e2f2eaf1a79013f90043251929f
Joris Ros <j.ros@youwe.nl>
2019-08-26 11:56:16 +0200

    Implement the mode parameter and add functionality to reteive files

--- Service/FileTransferService.php
+++ Service/FileTransferService.php
@@ -2,13 +2,18 @@
 
 namespace FileTransferBundle\Service;
 
+use http\Exception\RuntimeException;
 use phpseclib\Net\SFTP;
 use Pimcore\Log\ApplicationLogger;
 
 class FileTransferService
 {
+    const MODE_PUT = 'put';
+    const MODE_GET = 'get';
+
     private $logger;
     private $config;
+    private $mode;
 
     public function __construct(array $parameters, ApplicationLogger $logger)
     {
@@ -16,6 +21,17 @@ class FileTransferService
         $this->logger = $logger;
         $this->logger->setComponent('FileTransfer');
 
+        $this->mode = self::MODE_PUT;
+    }
+
+    /**
+     * Set the mode where the service should be operating
+     *
+     * @param string $mode
+     */
+    public function setMode(string $mode = self::MODE_PUT):void
+    {
+        $this->mode = $mode;
     }
 
     public function transferFile($serverid, $sourcefile, $targetfile)
@@ -46,7 +62,17 @@ class FileTransferService
 
         $this->changeToDirectory($sftpFolderName);
 
-        $this->putToServer($sftp, $targetfile, $sourcefile);
+        switch ($this->mode) {
+            case FileTransferService::MODE_PUT:
+                $this->putToServer($sftp, $targetfile, $sourcefile);
+                break;
+            case FileTransferService::MODE_GET:
+                $this->getFromServer($sftp, $targetfile, $sourcefile);
+                break;
+            default:
+                throw new \RuntimeException("There is no mode selected please use -m or --method");
+                break;
+        }
     }
 
     /**
@@ -94,6 +120,22 @@ class FileTransferService
     private function putToServer(SFTP $sftp, string $targetFile, string $sourceFile): void
     {
         if (!$sftp->put($targetFile, $sourceFile, SFTP::SOURCE_LOCAL_FILE)) {
+            $e = "Couldn't send file to sftp. " . $sftp->getLastSFTPError();
+            $this->logger->error($e);
+            throw new \RuntimeException($e);
+        }
+    }
+
+    /**
+     * Download the file from the remote server
+     *
+     * @param SFTP $sftp
+     * @param string $targetFile
+     * @param string $sourceFile
+     */
+    private function getFromServer(SFTP $sftp, string $targetFile, string $sourceFile):void
+    {
+        if (!$sftp->get($targetFile, $sourceFile, SFTP::SOURCE_LOCAL_FILE)) {
             $e = "Couldn't send file to sftp. " . $sftp->getLastSFTPError();
             $this->logger->error($e);
             throw new \RuntimeException($e);
